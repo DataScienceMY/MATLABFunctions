dbe613c3230c22f510e736d868b5ea0d5afbee5c
Adib Yusof <mkhairuladibmyusof@gmail.com>
2020-09-20 15:46:39 +0800

    Automatically add alphabetical label on each panel

--- nicefigure.m
+++ nicefigure.m
@@ -5,10 +5,10 @@ function nicefigure(Opts)
 % Syntax
 %   nicefigure              :  Transform all current figures at once
 %
-%   Name-value pairs (granular customization)
-%       -- Selection fo figures
+%   Name-value pairs (for granular customization)
+%       -- Selection for figures
 %       FigHandle           : scalar figure handle (default: all active figures)
-%       -- Font size for individual item 
+%       -- Font size (refer the arguments block in the code to see default values)
 %       TextSize            : scalar double 
 %       TickSize            : scalar double 
 %       LabelSize           : scalar double 
@@ -19,29 +19,51 @@ function nicefigure(Opts)
 %       TextboxSize         : scalar double 
 %       HeatmapSize         : scalar double 
 %       ColorbarSize        : scalar double
+%       >> If you are using math equations in any parts of your figure, make sure they are enclosed within $...$ and follow LaTeX syntax, e.g., $E = mc^{2}$ 
+%       -- Show alphabetical panel labels
+%       PanelLabel          : scalar logical (default: true)
 %   
-%   >> If you are using math equations in any parts of your figure, make sure they are enclosed within $...$ and follow LaTeX syntax, e.g., $E = mc^{2}$ 
-%
 % Written by Adib Yusof (2020) | mkhairuladibmyusof@gmail.com
 
 arguments
     Opts.FigHandle = findall(0, 'Type', 'figure')
-    Opts.TextSize (1,1) double = 18
+    Opts.TextSize (1,1) double = 17
     Opts.TickSize (1,1) double = 17
-    Opts.LabelSize (1,1) double = 18
-    Opts.TitleSize (1,1) double = 20
-    Opts.TextLegendSize (1,1) double = 15
-    Opts.TitleLegendSize (1,1) double = 16
-    Opts.ConsLineSize (1,1) double = 18
-    Opts.TextboxSize (1,1) double = 18
+    Opts.LabelSize (1,1) double = 17
+    Opts.TitleSize (1,1) double = 18
+    Opts.TextLegendSize (1,1) double = 16
+    Opts.TitleLegendSize (1,1) double = 17
+    Opts.ConsLineSize (1,1) double = 17
+    Opts.TextboxSize (1,1) double = 17
     Opts.HeatmapSize (1,1) double = 17
     Opts.ColorbarSize (1,1) double = 17
+    Opts.PanelLabel (1,1) logical = true
 end
 
 AllText = findall(Opts.FigHandle, 'Type', 'text');
 set(AllText, 'Interpreter', 'latex', 'FontSize', Opts.TextSize);
 
 AllAxes = findall(Opts.FigHandle, 'Type', 'axes');
+
+if Opts.PanelLabel
+    for i = 1:numel(Opts.FigHandle)
+        CurrentFig = Opts.FigHandle(i);
+        ExistingLabel = findall(CurrentFig, 'Tag', 'PanelLabel');
+        delete(ExistingLabel);
+        AxInFig = vertcat( findall(CurrentFig, 'Type', 'axes'), findall(CurrentFig, 'Type', 'heatmap') ) ;
+        LetterCode = 97;        % Code 97 corresponds to letter 'a' in ASCII
+        for j = 1:numel(AxInFig)
+            AxPosition = AxInFig(j).Position;
+            AxInset = AxInFig(j).TightInset;
+            LblPosition = [AxPosition(1)-1.1*AxInset(1) AxPosition(2)+0.88*AxPosition(4) 0.04 0.04 ];     % [left, bottom, width, height]
+            LblText = ['\textbf{(', LetterCode, ')}'];
+            annotation(CurrentFig, 'textbox', 'String', LblText, 'Position', LblPosition, 'Units', 'normalized', 'LineStyle', 'none', 'Interpreter', 'latex',...
+                'FitBoxToText', 'on', 'VerticalAlignment', 'middle', 'HorizontalAlignment', 'center', 'Tag', 'PanelLabel');
+            LetterCode = LetterCode + 1;
+        end
+    end
+end
+
 for i = 1:numel(AllAxes)
     set(AllAxes(i), 'TickLabelInterpreter', 'latex', 'FontSize', Opts.TickSize, 'box', 'on')
     set(AllAxes(i).XLabel, 'FontSize', Opts.LabelSize);
@@ -75,5 +97,7 @@ if ~ isempty(AllColorbar)
         AllColorbar(i).TickLabelInterpreter = 'latex';
     end
 end
+
+    
 end
 
